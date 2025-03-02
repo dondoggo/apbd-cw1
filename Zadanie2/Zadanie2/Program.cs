9b54143ddc470069a23cc660a8eb75f12c978cb7
Mateusz Drabik <s27004@pjwstk.edu.pl>
2025-03-02 16:40:33 +0100

    Modyfikacja 1

--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -12,7 +12,20 @@ class Program
         Console.WriteLine("Podaj swój wiek:");
         int wiek = int.Parse(Console.ReadLine());
 
-        // Wyświetlenie powitania
-        Console.WriteLine($"Witaj, {imie}! Masz {wiek} lat.");
+        // Pobranie ulubionego koloru od użytkownika
+        Console.WriteLine("Podaj swój ulubiony kolor:");
+        string kolor = Console.ReadLine();
+
+        // Pobranie hobby od użytkownika
+        Console.WriteLine("Jakie jest Twoje hobby?");
+        string hobby = Console.ReadLine();
+
+        // Kalkulacja - ile lat użytkownik będzie miał za 10 lat
+        int wiekZa10Lat = wiek + 10;
+
+        // Wyświetlenie powitania i dodatkowych informacji
+        Console.WriteLine($"\nWitaj, {imie}!");
+        Console.WriteLine($"Masz {wiek} lat i za 10 lat będziesz miał {wiekZa10Lat} lat.");
+        Console.WriteLine($"Twój ulubiony kolor to {kolor}, a Twoje hobby to {hobby}.");
     }
 }
