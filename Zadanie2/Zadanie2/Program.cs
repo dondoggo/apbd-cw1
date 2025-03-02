12fad15ffcbd2533a3856bc9ba1a0d2be7944a6e
Mateusz Drabik <s27004@pjwstk.edu.pl>
2025-03-02 16:39:31 +0100

    New change

--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Pobranie imienia od użytkownika
+        Console.WriteLine("Podaj swoje imię:");
+        string imie = Console.ReadLine();
+
+        // Pobranie wieku od użytkownika
+        Console.WriteLine("Podaj swój wiek:");
+        int wiek = int.Parse(Console.ReadLine());
+
+        // Wyświetlenie powitania
+        Console.WriteLine($"Witaj, {imie}! Masz {wiek} lat.");
+    }
+}
